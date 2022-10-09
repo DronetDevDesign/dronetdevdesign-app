62469d5170cda8088f8d2b3113d4ad798b30954e
Ron Dronet <ron@dronetdevdesign.com>
2022-10-09 13:33:52 -0500

    creating footer component and css styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import './FooterStyles.css';
 import React from 'react';
-import { FaHome, FaLinkedin, FaFacebook } from 'react-icons/fa';
+import { FaHome, FaLinkedin, FaFacebook, FaPhone, FaMailBulk } from 'react-icons/fa';
 
 const Footer = () => {
   return (
@@ -8,21 +8,29 @@ const Footer = () => {
       <div className='footer-container'>
         <div className='contact'>
           <div className='address'>
-            <FaHome size={20} style={{color: '#DFE2E6', marginRight: '32px'}} />
+            <div className='icons'>
+              <FaHome size={25} style={{ color: '#8A66DB', marginRight: '8px' }} />
+            </div>
             <div>
-              <p>Houston, Texas</p>
-              <p>United States of America</p>
+              <p>Houston, Texas &nbsp; | &nbsp; United States of America</p>
             </div>
           </div>
-          <div className='contact-me'></div>
-          <p>Please use the contact form to get in touch with me. Due to scammers and spam-bots I will not reveal my phone number or email address here. I apologize for the inconvenience.</p>
+          <div className='contact-section'>
+            <div className='contact-me'>
+              <div className='icons'>
+                <FaPhone size={22} style={{ color: '#8A66DB', marginRight: '8px' }} />
+                <FaMailBulk size={25} style={{ color: '#8A66DB', marginRight: '8px' }} />
+              </div>
+              <p>Please use the contact form to get in touch with me. Due to scammers and spam-bots I will not reveal my phone number or email address here. I apologize for the inconvenience.</p>
+            </div>
+          </div>
         </div>
         <div className='social-media'>
-          <h4>Check out my social media profiles:</h4>
           <div className='social-icons'>
-            <FaFacebook size={30} style={{ color: '#DFE2E6', marginRight: '16px' }} />
-            <FaLinkedin size={30} style={{ color: '#DFE2E6', marginRight: '32px' }} />
+            <FaFacebook size={25} style={{ color: '#8A66DB', marginRight: '10px' }} />
+            <FaLinkedin size={25} style={{ color: '#8A66DB', marginRight: '10px' }} />
           </div>
+          <h4>Check me out on social media</h4>
         </div>
       </div>
     </div>
