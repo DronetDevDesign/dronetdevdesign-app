730e0967e6e79ec720b1460e1586c97dde5451a7
Ron Dronet <ron@dronetdevdesign.com>
2022-10-12 16:50:39 -0500

    troubleshooting git pages launch

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
-console.log(123);
+console.log(333);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter basename={window.location.pathname || ''}>
