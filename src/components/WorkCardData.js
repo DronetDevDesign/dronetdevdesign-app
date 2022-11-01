7b0254092cabca1eb91f1fca2f7e640fb90b6adf
Ron Dronet <ron@dronetdevdesign.com>
2022-11-01 13:27:34 -0500

    adding new code to add the birthday Plan It app to website

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -1,20 +1,28 @@
-import projectOne from '../assets/mood-flix.jpg';
-import projectTwo from '../assets/art-garage.jpg';
-import projectThree from '../assets/weather-bee.jpg';
-import projectFour from '../assets/mercedes-binge.jpg';
-import projectFive from '../assets/krafty-recipes.jpg';
+import projectOne from '../assets/birthday-plan-it-screenshot.jpg';
+import projectTwo from '../assets/mood-flix.jpg';
+import projectThree from '../assets/art-garage.jpg';
+import projectFour from '../assets/weather-bee.jpg';
+import projectFive from '../assets/mercedes-binge.jpg';
 
 const ProjectCardData = [
   {
     imagesource: projectOne,
-    title: 'Mood Flix',
-    text: "Ever wonder what it would be like to choose a movie depending on what type of mood you're in? Well, MoodFlix can help! Simply select a MoodMoji from the dropdown menu to search movies that fit your mood.",
-    website: 'https://dronetdevdesign.github.io/Mood-Flix/',
-    git: 'https://github.com/DronetDevDesign/Mood-Flix',
+    title: 'Birthday Plan It',
+    text: "Welcome to the Birthday Plan It app where we help you plan your birthday party by creating a cool invitation. Simply sign up to create an account then pick a theme from the thumbnails below!",
+    website: 'https://birthdayplanit.herokuapp.com/',
+    git: 'https://github.com/violanerd/birthday-plan-it',
     enternal: false 
   },
   {
     imagesource: projectTwo,
+    title: 'Mood Flix',
+    text: "Here at Art Garage we want to connect artists with potential buyers who are passionate about art. Artists are able to create a profile by introducing themselves by creating a profiling with their info and website link.",
+    website: 'https://dronetdevdesign.github.io/Mood-Flix/',
+    git: 'https://github.com/DronetDevDesign/Mood-Flix',
+    enternal: false
+  },
+  {
+    imagesource: projectThree,
     title: 'Art Garage',
     text: "Here at Art Garage we want to connect artists with potential buyers who are passionate about art. Artists are able to create a profile by introducing themselves by creating a profiling with their info and website link.",
     website: 'https://art-garage-app.herokuapp.com/',
@@ -22,7 +30,7 @@ const ProjectCardData = [
     enternal: false
   },
   {
-    imagesource: projectThree,
+    imagesource: projectFour,
     title: 'Weather Bee',
     text: "The weather has been quite the buzz lately! Find out why on WeatherBee. Fun and easy way to check out the weather if you're going to travel or have outdoor activities planned. This app is perfect in case you're taking a trip!",
     website: 'https://dronetdevdesign.github.io/weather-bee/',
@@ -30,17 +38,9 @@ const ProjectCardData = [
     enternal: false
   },
   {
-    imagesource: projectFour,
+    imagesource: projectFive,
     title: 'Mercedes Binge',
     text: "The perfect app for Mecedes lovers! This app taps into an API that fetches the most beautiful photos that Mercedes Benz has to offer. Kick back and dream as if you owned one of these beauties or make your wish list to buy one.",
-    website: '/',
-    git: '/',
-    enternal: true
-  },
-  {
-    imagesource: projectFive,
-    title: 'Krafty Recipes',
-    text: "Who does't love Kraft cheese? In this app you'll be able to search through hundreds of recepes that use Kraft cheese. There are also photos is case you want to drool over cheese dishes while at work. come inside and take a look.",
     website: '/',
     git: '/',
     enternal: true
