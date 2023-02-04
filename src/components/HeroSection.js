77e20faf1607ae8802c61900808669ad583f60aa
Ron Dronet <ron@dronetdevdesign.com>
2023-02-04 13:14:26 -0600

    replacing background image

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,21 +1,19 @@
 import './HeroSectionStyles.css';
 
 import React from 'react'
-import IntroImage from '../assets/hero-section-bkgd.jpg';
+// import IntroImage from '../assets/hero-section-bkgd.jpg';
 import { Link } from 'react-router-dom';
 
 const HeroSection = () => {
   return (
     <div className='hero-section'>
-      <div className='intro-image'>
-        <img src={IntroImage} alt='IntroImage' />
-      </div>
       <div className='hero-content'>
-        <h4 className='subtitle'>My superpower is:</h4>
+        <h4 className='subtitle'>Hi, my name is Ron Dronet and my passion is:</h4>
         <h1>Capturing Creativity By Integrating Full-stack Development and Graphic Design.</h1>
         <Link to='/portfolio' className='portfolio-button'>Portfolio</Link>
         <Link to='/contact' className='contact-button'>Contact</Link>
       </div>
+      <div className='image-container'></div>
     </div>
   )
 }
