0eab7b7aa8173062d82d0162c7f8371dc69d413d
Ron Dronet <ron@dronetdevdesign.com>
2023-04-29 13:04:41 -0500

    coding CSS animation for landing page

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,7 +1,26 @@
 import './HeroSectionStyles.css';
 
 import React from 'react'
-import IntroImage from '../assets/dronet-illustration-no-background.png';
+import IntroImage from '../assets/main-front.png';
+import HeadImage from '../assets/main-head.png';
+import GlowImage from '../assets/main-yellow-glow.png';
+import LightsOne from '../assets/main-lights-01.png';
+import LightsTwo from '../assets/main-lights-02.png';
+import Bubbles from '../assets/main-bubbles.png';
+import JavascriptIcon from '../assets/main-javascript.png';
+import IllustratorIcon from '../assets/main-illustrator.png';
+// import AffinityImage from '../assets/main-affinity.png';
+// import AffinityImage from '../assets/main-affinity.png';
+// import AffinityImage from '../assets/main-affinity.png';
+// import AffinityImage from '../assets/main-affinity.png';
+// import AffinityImage from '../assets/main-affinity.png';
+// import AffinityImage from '../assets/main-affinity.png';
+// import AffinityImage from '../assets/main-affinity.png';
+// import AffinityImage from '../assets/main-affinity.png';
+// import AffinityImage from '../assets/main-affinity.png';
+// import AffinityImage from '../assets/main-affinity.png';
+// import AffinityImage from '../assets/main-affinity.png';
+// import AffinityImage from '../assets/main-affinity.png';
 import { Link } from 'react-router-dom';
 
 const HeroSection = () => {
@@ -17,7 +36,28 @@ const HeroSection = () => {
         </div>
       </div>
         <div className='image-container'>
+          <div className='javascript-icon'>
+            <img src={JavascriptIcon} alt="javacsript icon"></img>
+          </div>
+          <div className='illustrator-icon'>
+            <img src={IllustratorIcon} alt="illustrator icon"></img>
+          </div>
+          <div className='glow'>
+            <img src={GlowImage} alt="glow"></img>
+          </div>
+          <div className='bubbles'>
+            <img src={Bubbles} alt="glow"></img>
+          </div>
+          <div className='head'>
+            <img src={HeadImage} alt="head"></img>
+          </div>
           <img src={IntroImage} alt="capturing creativity illustration"></img>
+          <div className='lights-one'>
+            <img src={LightsOne} alt="head"></img>
+          </div>
+          <div className='lights-two'>
+            <img src={LightsTwo} alt="head"></img>
+          </div>
         </div>
     </div>
     </div>
