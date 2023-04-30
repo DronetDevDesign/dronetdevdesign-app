b1701f28162deba348b04ccc89f2ee81c8b0cc18
Ron Dronet <ron@dronetdevdesign.com>
2023-04-30 12:25:19 -0500

    coding CSS animation for landing page

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -14,13 +14,13 @@ import NodeIcon from '../assets/main-node.png';
 import ExpressIcon from '../assets/main-express.png';
 import CssIcon from '../assets/main-css3.png';
 import MongoIcon from '../assets/main-mongo-db.png';
-// import AffinityImage from '../assets/main-affinity.png';
-// import AffinityImage from '../assets/main-affinity.png';
-// import AffinityImage from '../assets/main-affinity.png';
-// import AffinityImage from '../assets/main-affinity.png';
-// import AffinityImage from '../assets/main-affinity.png';
-// import AffinityImage from '../assets/main-affinity.png';
-// import AffinityImage from '../assets/main-affinity.png';
+import HtmlIcon from '../assets/main-html5.png';
+import XdIcon from '../assets/main-xd.png';
+import PhotoshopIcon from '../assets/main-photoshop.png';
+import SketchIcon from '../assets/main-sketch.png';
+import AffinityIcon from '../assets/main-affinity.png';
+import IndesignIcon from '../assets/main-indesign.png';
+import BlenderIcon from '../assets/main-blender.png';
 import { Link } from 'react-router-dom';
 
 const HeroSection = () => {
@@ -56,6 +56,27 @@ const HeroSection = () => {
           </div>
           <div className='mongo-icon'>
             <img src={MongoIcon} alt="mongo icon"></img>
+          </div>
+          <div className='html-icon'>
+            <img src={HtmlIcon} alt="html icon"></img>
+          </div>
+          <div className='xd-icon'>
+            <img src={XdIcon} alt="xd icon"></img>
+          </div>
+          <div className='photoshop-icon'>
+            <img src={PhotoshopIcon} alt="photoshop icon"></img>
+          </div>
+          <div className='sketch-icon'>
+            <img src={SketchIcon} alt="sketch icon"></img>
+          </div>
+          <div className='affinity-icon'>
+            <img src={AffinityIcon} alt="affinity icon"></img>
+          </div>
+          <div className='indesign-icon'>
+            <img src={IndesignIcon} alt="indesign icon"></img>
+          </div>
+          <div className='blender-icon'>
+            <img src={BlenderIcon} alt="blender icon"></img>
           </div>
           <div className='glow'>
             <img src={GlowImage} alt="glow"></img>
